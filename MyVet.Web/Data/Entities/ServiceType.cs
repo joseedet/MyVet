28f285f50a5a2c7b8d3d7e6fa3206b90c93c0a2e
Jose Edet <joseedet@gmail.com>
2019-10-13 17:01:22 +0100

    Service Type views.

--- MyVet.Web/Data/Entities/ServiceType.cs
+++ MyVet.Web/Data/Entities/ServiceType.cs
@@ -10,7 +10,7 @@ namespace MyVet.Web.Data.Entities
     {
         public int Id { get; set; }
 
-        [Display(Name = "Pet Type")]
+        [Display(Name = "Service Type")]
         [MaxLength(50, ErrorMessage = "The {0} field can not have more than {1} characters.")]
         [Required(ErrorMessage = "The field {0} is mandatory.")]
         public string Name { get; set; }
